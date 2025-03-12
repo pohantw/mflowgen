bd9fe6b1410e62584fab052447e5ce738c491fc0
Po-Han Chen <pohan@stanford.edu>
2025-03-11 17:08:07 -0700

    making genlibdb hack node into its own node; remove blockages/halo after signoff

--- steps/cadence-innovus-flowsetup/innovus-foundation-flow/custom-scripts/summary-report.tcl
+++ steps/cadence-innovus-flowsetup/innovus-foundation-flow/custom-scripts/summary-report.tcl
@@ -8,3 +8,6 @@
 
 summaryReport -noHtml -outfile $vars(rpt_dir)/$vars(step).summaryReport.rpt
 
+# delete existing route blockage and halo to prevent false alarm in DRC/connectivity checking in innovus
+deleteRouteBlk -all
+deleteHaloFromBlock -allBlock
---
+++ steps/synopsys-ptpx-genlibdb-r7cad-hack/designer_interface.tcl
@@ -0,0 +1,54 @@
+#=========================================================================
+# designer_interface.tcl
+#=========================================================================
+# The designer_interface.tcl file is the first script run by PTPX (see the
+# top of ptpx.tcl). It is the interface that connects the scripts with
+# the following:
+#
+# - ASIC design kit
+# - Build system variables
+#
+# Author : Christopher Torng
+# Date   : May 20, 2019
+
+#-------------------------------------------------------------------------
+# Interface to the ASIC design kit
+#-------------------------------------------------------------------------
+
+set ptpx_additional_search_path   inputs/adk
+set ptpx_target_libraries [join "
+    [lsort [glob -nocomplain inputs/adk/stdcells*-$::env(corner).db]]
+"]
+set ptpx_extra_link_libraries [join "
+    [lsort [glob -nocomplain inputs/adk/*-$::env(corner).db]]
+    [lsort [glob -nocomplain inputs/*-$::env(corner).db]]
+"]
+
+# Remove any elements of target libraries froim extra_link_libraries
+set exclusion_result {}
+foreach elem $ptpx_extra_link_libraries {
+    if { [lsearch -exact $ptpx_target_libraries $elem] == -1 } {
+        lappend exclusion_result $elem
+    }
+}
+
+set ptpx_extra_link_libraries $exclusion_result
+
+#-------------------------------------------------------------------------
+# Interface to the build system
+#-------------------------------------------------------------------------
+
+set ptpx_design_name              $::env(design_name)
+
+set ptpx_flow_dir                 .
+set ptpx_plugins_dir              .
+set ptpx_logs_dir                 logs
+set ptpx_reports_dir              reports
+set ptpx_results_dir              results
+
+set ptpx_gl_netlist               [lsort [glob -nocomplain inputs/*.vcs.v]]
+set ptpx_sdc                      [lsort [glob -nocomplain inputs/*.pt.sdc]]
+set ptpx_spef                     [lsort [glob -nocomplain inputs/*.spef.gz]]
+
+puts "done"
+
